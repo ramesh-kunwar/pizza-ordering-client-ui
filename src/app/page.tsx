872be246c1c7d924ef159c40a20234f3824a48e7
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2025-08-01 15:22:18 +0545

    Add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { Button } from "@/components/ui/button";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      Welcome to client UI
+      <div>
+        <Button>Button</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
